3a0e2c27e033c94354d766c1ef5a5d1fa3b9ab6e
Andres Junge <ajunge@yaykuy.cl>
2018-04-03 01:43:12 -0300

    artifact-index

---
+++ artifact-index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    RestrictedTransferToken: {
+        latestVersion: 'v1',
+        v1: require('./build/contracts/RestrictedTransferToken.json'),
+      },
+    AllowanceRegistry: {
+      latestVersion: 'v1',
+      v1: require('./build/contracts/AllowanceRegistry.json'),
+    }
+}
