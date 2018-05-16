f3b605381dcb8c8b575323a2b018f546ae8496d7
Andres Junge <ajunge@yaykuy.cl>
2018-05-16 19:49:01 -0400

    resolves #13

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 const AllowanceRegistry = artifacts.require('AllowanceRegistry');
+const FeeSupplyController = artifacts.require('FeeSupplyController');
 const cCLP = artifacts.require('cCLP');
 
 const cCLPRegistryOwner_RINKEBY="0x4503d2cb92be591f4f4e650a953f7da10cb2ffe7"; //multisig key
 const cCLPSupplyController_RINKEBY="0x515f4508f0b287b3b953f120b472a9d346e6e616"; //multisig key
 
 const ajungeKey='0x9C803151d0fD38f8C9FCEe7D5d02498dF6067E5A';
+const deployerAddress='0x768d7e1c26D5CAc7e4A0cEFB324fcb42Ee5a931d' //must match .seed first addr.
 
 
 module.exports = function (deployer,network) {
@@ -20,10 +22,8 @@ module.exports = function (deployer,network) {
     return instance.transferOwnership(registryOwner);
   })
   .then(() => {
-    let supplyController=ajungeKey;
-    if (network == "rinkeby") {
-      supplyController=cCLPSupplyController_RINKEBY
-    }
+    let supplyController=deployerAddress;
+
     return deployer.deploy(
       cCLP, 
       supplyController,
@@ -39,6 +39,33 @@ module.exports = function (deployer,network) {
       tokenOwner=cCLPSupplyController_RINKEBY
     }
     return instance.transferOwnership(tokenOwner);
+  })
+  .then(() => {
+    let supplyController=ajungeKey;
+    if (network == "rinkeby") {
+      supplyController=cCLPSupplyController_RINKEBY
+    }
+
+    let beneficiaryAddress=ajungeKey;
+
+    return deployer.deploy(
+      FeeSupplyController, 
+      supplyController,
+      cCLP.address,
+      beneficiaryAddress
+    );
+  })
+  .then(() => {
+    return cCLP.deployed();
+  })
+  .then((instance) => {
+    return instance.changeSupplyController(FeeSupplyController.address);
+  })
+  .then(()=>{
+    console.log("DONE!")
+  })
+  .catch((err) => {
+    console.error(err);
   })
   
 }
